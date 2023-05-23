0eec171e9ff4510698512fd66db3357bd2084be2
ashel111 <21h09.ashel@sjec.ac.in>
2023-05-23 13:50:32 +0530

    Initial Commit

---
+++ weather app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather app
+//
+//  Created by Ashel D Souza on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
